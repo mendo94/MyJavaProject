93a5e6818c8d6704960c4d191820b1a17420d708
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-06 20:54:10 -0400

    variable practice

---
+++ src/unit1_fundamentals/Variables.java
@@ -0,0 +1,19 @@
+package unit1_fundamentals;
+
+public class Variables {
+    public static void main(String[] args) {
+        //primitive data types: byte, short, int, long, float, double, boolean, char
+        int guitar = 20;
+        System.out.println(guitar + 6);
+        shorty();
+    }
+
+    public static void shorty() {
+       int x = 15;
+       x =32;
+       x = -1000;
+       System.out.print(x);
+    }
+}
+
+
