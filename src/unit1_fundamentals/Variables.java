824479d02363b978fe2365d46a2f2053b4915b4c
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-07 11:59:33 -0400

    Writing descriptive variable names

--- src/unit1_fundamentals/Variables.java
+++ src/unit1_fundamentals/Variables.java
@@ -6,11 +6,33 @@ public class Variables {
         int guitar = 20;
         System.out.println(guitar + 6);
         shorty();
+        dataTypes();
+        descriptiveVariableNames();
     }
 
     public static void shorty() {
        System.out.println(15 / 2); // if written like this, then the output will not be a decimal
         System.out.println(15.0 / 2); // write the number as a double, so it does not round
+    }
+
+    public static void dataTypes() {
+       int a = 27;
+       double b = 14.27;
+       boolean c = false;
+       char d = '\u005A'; //capital z
+       System.out.println(d);
+       String e = "The sky is blue";
+       System.out.println(e); //string is many characters, char is just one.
+        String f = "Rina";
+        System.out.println("My name is " + f); //string is many characters, char is just one.
+    }
+
+    public static void descriptiveVariableNames() {
+      int age = 27;
+      double statueHeightInMeters = 14.27;
+      boolean hasDriversLicense = true;
+      String name = "fruit flies";
+      System.out.println("There are so many " + name);
     }
 }
 
