1950c902e5f9507b323923de96c2aa523eb9da0b
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-08 14:40:46 -0400

    importing the scanner

---
+++ src/unit2_fundamentals/userInput.java
@@ -0,0 +1,11 @@
+package unit2_fundamentals;
+//import the package file path
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(input);
+    }
+}
