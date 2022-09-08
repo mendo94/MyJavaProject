c5f1bf6c69b1bb4d143b0764be0e2b151e83c3be
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-08 14:47:07 -0400

    utilizing user inputs to write a small program

--- src/unit2_fundamentals/userInput.java
+++ src/unit2_fundamentals/userInput.java
@@ -7,5 +7,18 @@ public class userInput {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         System.out.println(input);
+        scanner();
+    }
+
+    public static void scanner() {
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String userName= scanner.next(); //used to read strings
+        System.out.println(userName + " is a fantastic name!");
+
+        System.out.println("How old are you?");
+        int userAge = scanner.nextInt(); //used to read integers
+        System.out.println("Your name is " + userName + " and you are " + userAge + " years old.");
     }
 }
+
