79a0fa96d90fecbb89450a9ade9a09d1885f761c
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-08 15:41:24 -0400

    The formatter allows to format decimals and inject values

--- src/unit2_fundamentals/formatMethod.java
+++ src/unit2_fundamentals/formatMethod.java
@@ -8,5 +8,17 @@ public class formatMethod {
         System.out.format("I have %d dog, %d cats, 1 %s.", 1, 0, "family\n"); //works just like print but you can use a format specifier
         // %d are for integers and %s are for strings
         System.out.format("\nI have %.3f dog, %d cats, 1 %s.", 7.8, 0, "family");
+        height();
+    }
+
+    public static void height() {
+        Scanner scanner = new Scanner(System.in);
+        double userHeight = scanner.nextDouble();
+        double averageMaleHeightDifference = userHeight - 1.75;
+        double averageFemaleHeightDifference = userHeight - 1.62;
+
+        System.out.println("\n The difference between your height and the average is: ");
+        System.out.format( "%.2fm if you are male \n", averageMaleHeightDifference );
+        System.out.format( "%.2fm if you are female \n", averageFemaleHeightDifference );
     }
 }
