10ebd42a4692d9d2fb100d979495755949b38db6
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-08 20:10:15 -0400

    introduction to conditionals in java

---
+++ src/unit2_fundamentals/ConditionalStatements.java
@@ -0,0 +1,27 @@
+package unit2_fundamentals;
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        System.out.print(1);
+        System.out.print(2);
+        System.out.print(3);
+
+        if (false) {
+            System.out.print(4);
+            System.out.print(5);
+            System.out.print(6);
+
+        }
+        System.out.print(7);
+        System.out.print(8);
+        System.out.print(9);
+
+    }
+
+    public static void height () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Cedar Point");
+    }
+}
