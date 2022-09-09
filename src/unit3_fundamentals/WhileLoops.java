3a3dea48c4e05266f7f0d82a685d6328f6e4c6ed
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-09 10:48:27 -0400

    Understanding the while loop in java

---
+++ src/unit3_fundamentals/WhileLoops.java
@@ -0,0 +1,16 @@
+package unit3_fundamentals;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int i = 0;
+        // will repeat until the statement is false
+        while (i < 10) {
+            System.out.println("Test");
+            i++;
+        }
+
+
+    }
+
+}
