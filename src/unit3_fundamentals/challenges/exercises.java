a92a3c931f56bc252d77fab33042b58124087011
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-09 13:48:32 -0400

    completed exercise 5

--- src/unit3_fundamentals/challenges/exercises.java
+++ src/unit3_fundamentals/challenges/exercises.java
@@ -1,6 +1,8 @@
 package unit3_fundamentals.challenges;
 
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class exercises {
     public static void main(String[] args) {
@@ -76,9 +78,26 @@ public class exercises {
 //
 //        You can use the line of code below which produces a random number from 1 to 100,
 //            and stores it in randomNumber.
+        Scanner scanner = new Scanner(System.in);
 
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, 101);
+        int guess;
 
-        }
+        System.out.println("--- Guess My Number Game ---");
 
+        do {
 
+            System.out.print("\nGuess: ");
+            guess = scanner.nextInt();
+
+            if (guess < randomNumber) {
+                System.out.println("HIGHER.");
+            } else if (guess > randomNumber) {
+                System.out.println("LOWER.");
+            } else {
+                System.out.println("CORRECT. My number was " + randomNumber + ".");
+            }
+
+        } while (guess != randomNumber);
+    }
 }
