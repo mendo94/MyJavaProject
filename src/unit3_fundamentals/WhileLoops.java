e38ed90f70c22259d98b03f62617ee8b9472b3a9
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-09 10:58:54 -0400

    using for loops with the break statement

--- src/unit3_fundamentals/WhileLoops.java
+++ src/unit3_fundamentals/WhileLoops.java
@@ -11,6 +11,26 @@ public class WhileLoops {
         }
 
 
+    forloop();
+        breakAndContinue();
+    }
+
+    public static void forloop() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i);
+
+        }
+    }
+    public static void breakAndContinue() {
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println();
+            System.out.println(i);
+        }
     }
 
 }
