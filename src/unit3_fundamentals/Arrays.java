9e8b1d501720fe6e33646a8fff0d489b33a6c308
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-11 10:04:13 -0400

    simple processing using arrays

--- src/unit3_fundamentals/Arrays.java
+++ src/unit3_fundamentals/Arrays.java
@@ -15,6 +15,25 @@ public class Arrays {
         for (int i = a.length -1; i >= 0; i--) {
             System.out.println(a[i]);
         }
+        processingArrays();
 
+    }
+
+    //only numbers greater than 100 will be printed.
+    public static void processingArrays() {
+        int[] nums = {1,2,3,4,5,6,7, 100, 200, 3000, 900};
+        int count = 0; //keep count of how many numbers are greater than 100.
+        int sumOfNums = 0; //find the mean or average
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 100) {
+                count++;
+                System.out.print(nums[i]);
+            }
+            sumOfNums = sumOfNums + nums[i];
+        }
+            System.out.println("Number of values greater than 100 are " + count);
+       double mean = (double)sumOfNums / nums.length;
+        System.out.println("The mean is" + mean);
     }
 }
