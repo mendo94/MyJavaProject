e4787f2b56ebc9ff81d6bf9a5977b37b446a76ed
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-11 10:47:47 -0400

    completed exercise 3

--- src/unit3_fundamentals/challenges/arrays.java
+++ src/unit3_fundamentals/challenges/arrays.java
@@ -17,8 +17,24 @@ public class arrays {
 //        Use a loop to populate an array with integers from 1 to 20.
 //        Use another loop to print the contents of the array (the elements) backwards.
         for (int i = foo.length - 1; i >= 0; i--) {
-            System.out.print(foo[i]);
+            System.out.println(foo[i]);
         }
+
+//        Exercise 3
+//
+//        Create an array of five doubles and give each element some value.
+//        Calculate the sum of all the elements and print the result.
+//                Calculate the mean/average of the all the elements and print that too.
+
+        double[] doubles = new double[] {1,2,3,4.22,5,55.55};
+        double sumOfElements = 0;
+
+        for (int i = 0; i < doubles.length; i++) {
+            sumOfElements = sumOfElements + doubles[i];
+            System.out.println(sumOfElements);
+        }
+        double mean = (double)sumOfElements / doubles.length;
+        System.out.println("the average is: " + mean);
 
     }
 }
