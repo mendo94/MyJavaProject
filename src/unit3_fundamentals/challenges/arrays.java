c33ab1017b8e4ca157c32f8f47e7ff4dd9ae1afb
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-11 10:34:51 -0400

    completed exercise 1

---
+++ src/unit3_fundamentals/challenges/arrays.java
@@ -0,0 +1,16 @@
+package unit3_fundamentals.challenges;
+
+public class arrays {
+    public static void main(String[] args) {
+//        Exercise 1
+//
+//        Create an integer array of 10 elements and give each element an
+//        arbitrary value. Print out the entire array using a loop.
+
+        int [] foo = {1,2,3,4,45, 6,7,8,9,10};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(foo[i]);
+        }
+
+    }
+}
