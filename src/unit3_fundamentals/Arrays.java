1cd847942fd824d0a1d3e0cc743a94ef7abfb5dd
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-11 09:45:02 -0400

    looping though the array backwards

--- src/unit3_fundamentals/Arrays.java
+++ src/unit3_fundamentals/Arrays.java
@@ -2,11 +2,18 @@ package unit3_fundamentals;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] b = {75, 29, 350, 7, 1929};
+        //iterate through an array
+        int[] b = {75, 29, 350, 7, 1929, 990};
         b[3] = 51;
         System.out.print(b[3]);
-        for (int i = 0 ; i < 5; i++) {
+        for (int i = 0 ; i < b.length; i++) {
             System.out.println(b[i]);
+        }
+
+        //looping through backwards
+        int[] a = {5, 75, 29, 350, 7, 1929, 990};
+        for (int i = a.length -1; i >= 0; i--) {
+            System.out.println(a[i]);
         }
 
     }
