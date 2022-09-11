1b659f8eea02da6dbeab6944e5e8191b4da1efe5
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-10 20:59:06 -0400

    learning to use arrays

---
+++ src/unit3_fundamentals/Arrays.java
@@ -0,0 +1,13 @@
+package unit3_fundamentals;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] b = {75, 29, 350, 7, 1929};
+        b[3] = 51;
+        System.out.print(b[3]);
+        for (int i = 0 ; i < 5; i++) {
+            System.out.println(b[i]);
+        }
+
+    }
+}
