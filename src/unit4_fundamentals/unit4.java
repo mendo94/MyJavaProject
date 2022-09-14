6caf9fae9f496ac5c690c90832bb54fe33c5798c
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-13 22:37:46 -0400

    working with methods

--- src/unit4_fundamentals/unit4.java
+++ src/unit4_fundamentals/unit4.java
@@ -2,6 +2,12 @@ package unit4_fundamentals;
 
 public class unit4 {
     public static void main(String[] args) {
+        foo();
+        foo();
+        System.out.println(19);
 
+    }
+    public static void foo() {
+        System.out.println(16);
     }
         }
