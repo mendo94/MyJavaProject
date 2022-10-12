ce96ac81cad1470cdcf9ee491cf1f6bfbd59144c
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:23:08 -0400

    conditional added to change users

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -9,11 +9,14 @@ public class TicTacToe {
 
         Scanner scanner = new Scanner(System.in);
         int input;
+        char turn = 'X';
+
 
         while (true) {
             System.out.print("Enter a position: ");
             input = scanner.nextInt();
-            pos[input - 1] = 'X';
+
+            pos[input - 1] = turn;
 
 
             System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
@@ -21,6 +24,12 @@ public class TicTacToe {
             System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
             System.out.println("---+---+---");
             System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
+
+            if (turn == 'X') {
+                turn = 'O';
+            } else if (turn == 'O') {
+                turn = 'X';
+            }
         }
     }
 }
