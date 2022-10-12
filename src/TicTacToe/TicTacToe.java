3b4c9ae1d2ec8f26d0a155d0dea61c50b4c13d64
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:17:57 -0400

    added a scanner and subtract one from input to place x on the right position

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,14 +1,24 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
-        char[] pos = {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        System.out.print("Enter a position: ");
+        input = scanner.nextInt();
+        pos[input - 1] = 'X';
+
 
         System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8]  + " " );
-        System.out.println("----+----+-----");
+        System.out.println("---+---+---");
         System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5]  + " " );
-        System.out.println("----+----+-----");
+        System.out.println("---+---+---");
         System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2]  + " " );
     }
 }
