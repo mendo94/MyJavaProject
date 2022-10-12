32dcc8b90f6d7124eef66687f3b18541260d0e6a
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:09:18 -0400

    setting up game board

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("    |    |    ");
+        System.out.println("----+----+-----");
+        System.out.println("    |    |    ");
+        System.out.println("----+----+-----");
+        System.out.println("    |    |    ");
+    }
+}
