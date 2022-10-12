03ed30546debf41a460d0647196419e56769a424
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:47:08 -0400

    modulus

--- src/PrimeNumbers/PrimeNumbers.java
+++ src/PrimeNumbers/PrimeNumbers.java
@@ -3,8 +3,12 @@ package PrimeNumbers;
 public class PrimeNumbers {
     public static void main(String[] args) {
 
-        for (int div == 0) {
-            System.out.println("Not prime");
+        for (int div = 2; div < 49; div++) {
+
+          if  (49 % div ==0) {
+                System.out.println("Not prime");
+
+            }
         }
     }
 }
