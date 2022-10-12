56a45611fbd2897ad181fbca9986eb6ad0475d13
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:39:11 -0400

    straight across pattern will win condtional

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -28,6 +28,18 @@ public class TicTacToe {
             System.out.println("---+---+---");
             System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
 
+            if (pos[0] == turn && pos[1] == turn && pos[2] == turn
+                    || pos[3] == turn && pos[4] == turn && pos[5] == turn
+                    || pos[6] == turn && pos[7] == turn && pos[8] == turn
+                    || pos[6] == turn && pos[3] == turn && pos[0] == turn
+                    || pos[7] == turn && pos[4] == turn && pos[1] == turn
+                    || pos[8] == turn && pos[5] == turn && pos[2] == turn
+                    || pos[6] == turn && pos[4] == turn && pos[2] == turn
+                    || pos[8] == turn && pos[4] == turn && pos[0] == turn
+            ) {
+                System.out.println(turn + " is the WINNNNNNNEERRR!!!!!!!! :D ");
+            }
+
             if (turn == 'X') {
                 turn = 'O';
             } else if (turn == 'O') {
