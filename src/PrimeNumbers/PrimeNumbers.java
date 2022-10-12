062794806643403a16ad4171aeb572182c2598dd
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:45:51 -0400

    conditional

--- src/PrimeNumbers/PrimeNumbers.java
+++ src/PrimeNumbers/PrimeNumbers.java
@@ -3,5 +3,8 @@ package PrimeNumbers;
 public class PrimeNumbers {
     public static void main(String[] args) {
 
+        for (int div == 0) {
+            System.out.println("Not prime");
+        }
     }
 }
