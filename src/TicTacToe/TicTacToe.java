7ff5b5d29ddf627e48538e9589bb1371e6cd7cd4
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:18:49 -0400

    Was placed into a while loop to continue asking for positions

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -10,15 +10,17 @@ public class TicTacToe {
         Scanner scanner = new Scanner(System.in);
         int input;
 
-        System.out.print("Enter a position: ");
-        input = scanner.nextInt();
-        pos[input - 1] = 'X';
+        while (true) {
+            System.out.print("Enter a position: ");
+            input = scanner.nextInt();
+            pos[input - 1] = 'X';
 
 
-        System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8]  + " " );
-        System.out.println("---+---+---");
-        System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5]  + " " );
-        System.out.println("---+---+---");
-        System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2]  + " " );
+            System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
+        }
     }
 }
