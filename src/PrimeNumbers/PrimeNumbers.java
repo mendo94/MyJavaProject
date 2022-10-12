62a6d7ab1d9eb149aedb620ffb7513d0ed37ec12
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:50:46 -0400

    boolean names start with the word is

--- src/PrimeNumbers/PrimeNumbers.java
+++ src/PrimeNumbers/PrimeNumbers.java
@@ -3,16 +3,22 @@ package PrimeNumbers;
 public class PrimeNumbers {
     public static void main(String[] args) {
 
-        int num = 9;
-        boolean isPrime;
+        int num = 1;
+        boolean isPrime = true;
 
         for (int div = 2; div < num; div++) {
 
           if  (num % div ==0) {
-                System.out.println("Not prime");
+
+                isPrime = false;
                 break;
 
             }
+        }
+        if (isPrime) {
+            System.out.println("Is prime");
+        } else {
+            System.out.println("Not prime");
         }
     }
 }
