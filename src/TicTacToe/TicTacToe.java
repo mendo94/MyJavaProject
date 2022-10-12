4465768c246d990cf7b9240cb6b40f7ae989d27f
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-11 21:14:38 -0400

    defining positions

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -2,10 +2,13 @@ package TicTacToe;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("    |    |    ");
+
+        char[] pos = {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+        System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8]  + " " );
         System.out.println("----+----+-----");
-        System.out.println("    |    |    ");
+        System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5]  + " " );
         System.out.println("----+----+-----");
-        System.out.println("    |    |    ");
+        System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2]  + " " );
     }
 }
